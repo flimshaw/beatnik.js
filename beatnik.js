c01db2516826ac381129c164db2f228c758ea810
Charlie <charlie.hoey@gmail.com>
2013-03-20 21:41:59 -0400

    initial commit

---
+++ beatnik.js
@@ -0,0 +1,11 @@
+// for each word in a sentence, pick a part of speech that can follow this word and then continue
+
+// a word class
+function Word(partOfSpeech) {
+		
+}
+
+// a sentence class
+function Sentence {
+	
+}
